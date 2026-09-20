a3989fac490f3a09e32ea438ca8dea41ee15a7a5
Sachin7137 <sachinaajage7137@gmail.com>
2026-09-20 12:19:09 +0530

    Add openRouter configuration

--- config/openRouter.js
+++ config/openRouter.js
@@ -1,11 +1,11 @@
-import {OpenRouter} from "@openrouter/sdk";
+import { OpenRouter } from "@openrouter/sdk";
 
-if(!process.env.OPENROUTER_API_KEY){
-    throw new Error("Open router API key is missing!");
+if (!process.env.OPENROUTER_API_KEY) {
+  throw new Error("Open router API key is missing!");
 }
 
 const openRouter = new OpenRouter({
-    apiKey: process.env.OPENROUTER_API_KEY
-})
+  apiKey: process.env.OPENROUTER_API_KEY,
+});
 
 export default openRouter;
